1ff9948ac67229613628ca85883fcb7fcc67baf5
ArminFarzam <armin.frzm72@gmail.com>
2023-12-12 14:57:00 +0330

    Add project files.

---
+++ Data/ProductManager.Data/Context/ProductManagerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManager.Data.Context;
+
+public class ProductManagerContext:IdentityDbContext
+{
+    public ProductManagerContext(DbContextOptions<ProductManagerContext> options):base(options)
+    {
+        
+    }
+
+
+    uuytuytty
+}
