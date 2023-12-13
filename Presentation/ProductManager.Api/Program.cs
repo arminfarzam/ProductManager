9d12986c8e14520cdb4ad324f7b406d591961785
ArminFarzam <armin.frzm72@gmail.com>
2023-12-13 23:06:53 +0330

    handle Jwt Authentication For Swagger

--- Presentation/ProductManager.Api/Program.cs
+++ Presentation/ProductManager.Api/Program.cs
@@ -6,6 +6,7 @@ using ProductManager.Application.DTOs.Configuration;
 using ProductManager.Data.Context;
 using ProductManager.IoC.Container;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+#region Swagger
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ProductManager API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+#endregion
+
 builder.Services.UseIoC(builder.Configuration);
 builder.Services.Configure<JwtConfigurationDto>(builder.Configuration.GetSection("JWT"));
 
